75a78b5e877fc7b490a26f200253ef7877ee1db0
SergeyFedos <feddoseev@gmail.com>
2025-06-22 23:42:10 +0800

    m

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,43 @@
-console.log('Task Manager App')
+require('./db/connect')
+const express = require('express')
+const app = express();
+const tasks = require('./routes/tasks');
+const connectDB = require('./db/connect');
+require('dotenv').config()
+// since we'll be sending json from our app, since we want to acces that data in our routes we need get middleware that is built in express, (if we dont't use this, then we won't have data in req.body )
+app.use(express.json()); 
+app.use(express.static('./public'))
+
+
+
+// routes
+
+
+app.use('/api/v1/tasks', tasks)
+
+
+
+
+
+
+const port = 3000;
+
+// we set up fuction start if connection to db is succesfull only then we start the server
+
+const start = async() => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`server is listening on port ${port}...`))
+  } catch (error) {
+    console.log(error)
+  }
+
+}
+
+start();
+
+// app.get('/api/v1/tasks')  -- get all the tasks
+// app.post('/api/v1/tasks')  -- create a new task 
+// app.get('/api/v1/tasks/:id')  -- get single task
+// app.patch('/api/v1/tasks/:id')  -- update task
+// app.delete('/api/v1/tasks/:id')  -- delete task
